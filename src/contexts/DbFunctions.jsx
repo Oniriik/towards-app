8326adfc203c141743f79392327ce4e73b897343
unknown <timot5924@gmail.com>
2022-06-07 13:25:42 +0200

    fix last push

--- src/contexts/DbFunctions.jsx
+++ src/contexts/DbFunctions.jsx
@@ -2,7 +2,7 @@
 import React, { useContext } from 'react'
 import firebase from 'firebase/compat/app'
 import { db } from '../config/firebase'
-import { useAuth } from './AuthProvider'
+// import { useAuth } from './AuthProvider'
 import { collection, query, where, getDocs } from "firebase/firestore";
 
 const dbContext = React.createContext()
@@ -13,12 +13,12 @@ export function useDbFunctions() {
 
 export function DbFunctions({ children }) {
 
-    const { currentUser } = useAuth()
+    // const { currentUser } = useAuth()
 
     function createUserProfile(infos, uid) {
         db.collection('users').doc(uid).set({
             ...infos,
-            signupDate: firebase.firestore.Timestamp.fromDate(new Date)
+            signupDate: firebase.firestore.Timestamp.fromDate(new Date())
         })
     }
 
